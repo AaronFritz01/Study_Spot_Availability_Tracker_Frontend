679fea4491fd408a0bc3a5e72b8b28faf8453d05
Aaron <fritzam@purdue.edu>
2022-03-21 19:31:05 -0400

    feat: uploading images for home view

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    server: {
-        port: 8300,
+  plugins: [vue()],
+  server: {
+    port: 8300,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src",
-                import.meta.url)),
-        },
-    },
+  },
 });
