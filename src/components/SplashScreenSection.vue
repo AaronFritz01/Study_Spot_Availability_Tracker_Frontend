0801caa3d278023c23381bb0d2c460ba4479f0c7
Aaron <fritzam@purdue.edu>
2022-03-21 20:58:38 -0400

    feat: added html for splash screen

--- src/components/SplashScreenSection.vue
+++ src/components/SplashScreenSection.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+import SplashButton from './templates/SplashButton.vue';
+</script>
 
 <template>
   <img alt="WALC" class="logo" src="@/assets/WALC_Background_Front_Page.jpg" />
+  <div class="transparent-rectangle">
+    <h2>Welcome</h2>
+    <h4>To the Purdue Study Spot Tracker</h4>
+    <div class="button-spacer">
+      <SplashButton />
+    </div>
+  </div>
 </template>
 
 <style></style>
