ff583029ef2b308debdeee8aae5dc4ec868b72ed
Aaron <fritzam@purdue.edu>
2022-03-21 21:32:30 -0400

    feat: add about us section html

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import SplashScreenSection from "../components/SplashScreenSection.vue";
 import OptionCardSection from "../components/OptionCardSection.vue";
 import AnnouncmentsSection from "../components/AnnouncmentsSection.vue";
+import AboutSection from "../components/AboutSection.vue";
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import AnnouncmentsSection from "../components/AnnouncmentsSection.vue";
     <SplashScreenSection />
     <OptionCardSection />
     <AnnouncmentsSection />
+    <AboutSection />
   </main>
 </template>
 
