6e91e947c1faeaa82da8f73ec089e5436e97084c
Aaron <fritzam@purdue.edu>
2022-03-21 20:49:43 -0400

    style: removed link highlighting and lint

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,39 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <header>
     <div class="gold-stripe"></div>
-    <div class="black-stripe"><img alt="Purdue logo" class="logo" src="../src/assets/Purdue University Logo.svg" /></div>
+    <div class="black-stripe">
+      <img
+        alt="Purdue logo"
+        class="logo"
+        src="../src/assets/Purdue University Logo.svg"
+      />
+    </div>
     <div class="wrapper">
       <nav class="main-nav">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/About">About</RouterLink>
-        <RouterLink to="/Campus_Map">Campus Map</RouterLink>
-        <RouterLink to="/Study_Spaces">Study Spaces</RouterLink>
-        <RouterLink to="/Statistics">Statistics</RouterLink>
-        <RouterLink to="/Contact">Contact</RouterLink>
+        <RouterLink style="text-decoration: none; color: inherit" to="/"
+          >Home</RouterLink
+        >
+        <RouterLink style="text-decoration: none; color: inherit" to="/About"
+          >About</RouterLink
+        >
+        <RouterLink
+          style="text-decoration: none; color: inherit"
+          to="/Campus_Map"
+          >Campus Map</RouterLink
+        >
+        <RouterLink
+          style="text-decoration: none; color: inherit"
+          to="/Study_Spaces"
+          >Study Spaces</RouterLink
+        >
+        <RouterLink
+          style="text-decoration: none; color: inherit"
+          to="/Statistics"
+          >Statistics</RouterLink
+        >
+        <RouterLink style="text-decoration: none; color: inherit" to="/Contact"
+          >Contact</RouterLink
+        >
       </nav>
     </div>
   </header>
@@ -22,15 +46,15 @@ import { RouterLink, RouterView } from "vue-router";
 </template>
 
 <style>
-.gold-stripe{
-  background-color: #6D5631;
+.gold-stripe {
+  background-color: #6d5631;
   height: 25px;
 }
-.main-nav{
-  background-color: #1E2A32;
+.main-nav {
+  background-color: #1e2a32;
   color: white;
 }
-.black-stripe{
+.black-stripe {
   background-color: black;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,40 +7,39 @@ import StatisticsView from "../views/StatisticsView.vue";
 import ContactView from "../views/ContactView.vue";
 
 const router = createRouter({
-    history: createWebHistory(
-        import.meta.env.BASE_URL),
-    routes: [{
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
-            path: "/About",
-            name: "about",
-            component: AboutView,
-        },
-        {
-            path: "/Campus_Map",
-            name: "campus_map",
-            component: CampusMapView,
-        },
-        {
-            path: "/Study_Spaces",
-            name: "study_spaces",
-            component: StudySpacesView,
-        },
-        {
-            path: "/Statistics",
-            name: "statistics",
-            component: StatisticsView,
-        },
-        {
-            path: "/Contact",
-            name: "contact",
-            component: ContactView,
-        },
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/About",
+      name: "about",
+      component: AboutView,
+    },
+    {
+      path: "/Campus_Map",
+      name: "campus_map",
+      component: CampusMapView,
+    },
+    {
+      path: "/Study_Spaces",
+      name: "study_spaces",
+      component: StudySpacesView,
+    },
+    {
+      path: "/Statistics",
+      name: "statistics",
+      component: StatisticsView,
+    },
+    {
+      path: "/Contact",
+      name: "contact",
+      component: ContactView,
+    },
+  ],
 });
-
 
 export default router;
