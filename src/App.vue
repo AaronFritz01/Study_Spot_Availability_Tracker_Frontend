407c48a2856124d3eb4f1fc462152203c4302ee8
Aaron <fritzam@purdue.edu>
2022-03-21 20:30:44 -0400

    feat: added correct logo

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
+    <div class="black-stripe"><img alt="Purdue logo" class="logo" src="../src/assets/Purdue University Logo.svg" /></div>
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
