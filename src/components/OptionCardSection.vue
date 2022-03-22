bafe1b87be269ba17f450da9a94223049a239cd4
Aaron <fritzam@purdue.edu>
2022-03-21 21:17:53 -0400

    feat: add option card selection section

---
+++ src/components/OptionCardSection.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="background-rectangle">
+    <div class="separated-rectangle">
+        <div class="card-spacer">
+            <div class="card-background">
+                <div href="" class="card-separated-rectangle">
+                    <p>Campus Map</p>
+                    <div class="decorative-bar"></div>
+                    <img src="../assets/Map_Icon.png" alt="Campus Map">
+                </div>
+            </div>
+            <div class="card-background">
+                <div href="" class="card-separated-rectangle">
+                    <p>Study Spaces</p>
+                    <div class="decorative-bar"></div>
+                    <img src="../assets/Book_Icon.png" alt="Study Spaces">
+                </div>
+            </div>
+            <div class="card-background">
+                <div href="" class="card-separated-rectangle">
+                    <p>Statistics</p>
+                    <div class="decorative-bar"></div>
+                    <img src="../assets/Bar_Graph_Icon.png" alt="Statistics">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import SplashScreenSection from "../components/SplashScreenSection.vue";
+import OptionCardSection from "../components/OptionCardSection.vue";
 </script>
 
 <template>
   <main>
     <SplashScreenSection />
+    <OptionCardSection />
   </main>
 </template>
