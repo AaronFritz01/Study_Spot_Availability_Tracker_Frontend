fa66fbc75ad920e710891df1dfbf086ac50f729e
Aaron <fritzam@purdue.edu>
2022-03-28 23:41:06 -0400

    feat: added temporary study space section

---
+++ src/components/LocationSection.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="columns">
+    <div class="left-column">
+      
+    </div>
+    <div class="right-column">
+      <table class="availability-table">
+  <thead>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Row 1, Cell 1</td>
+      <td>Row 1, Cell 2</td>
+    </tr>
+    <tr>
+      <td>Row 2, Cell 1</td>
+      <td>Row 2, Cell 2</td>
+    </tr>
+    <tr>
+      <td>Row 3, Cell 1</td>
+      <td>Row 3, Cell 2</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+table.customTable {
+  width: 100%;
+  background-color: #FFFFFF;
+  border-collapse: collapse;
+  border-width: 2px;
+  border-color: #7EA8F8;
+  border-style: solid;
+  color: #000000;
+}
+
+table.customTable td, table.customTable th {
+  border-width: 2px;
+  border-color: #7EA8F8;
+  border-style: solid;
+  padding: 5px;
+}
+
+table.customTable thead {
+  background-color: #7EA8F8;
+}
+</style>
---
+++ src/views/StudySpacesView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ContactSection from "../components/ContactSection.vue";
+import LocationSection from "../components/LocationSection.vue";
+</script>
+
+<template>
+  <main>
+      <LocationSection />
+    <ContactSection />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
